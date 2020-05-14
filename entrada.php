48af6ce3e7ef0f5c0c07b1c956e8bb3b848464da
hugoecarl <mormeme@gmail.com>
2020-05-14 19:36:15 -0300

    inicio rot8

--- entrada.php
+++ entrada.php
@@ -1,28 +1,5 @@
 <?php  
-$x = 2;
-$y = 5;
-echo "oi";
-$n = readline();
-$i = 0;
+1+1;
 
-  while (($i < $n) or ($i == $n)) {
-    if ($x > $y)
-      $y = $y + 1;
-    else if ($x < $y) {
-      $x = $x + 1;
-    }
-    else if ($x == $y) {
-            $x = $x + $i;
-            $y = $y + $i;
-            $i = $i + 1;
-        }
-  echo $i;
-  echo $x;
-  echo $y;
-  echo 888888;
-    }
-    $t = "abcde";
-    $f = "fg";
-    echo 25 and true;
-    echo "99" == "99";
+
 ?>

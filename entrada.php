801edd6b323640f82eff217b3ed0b8884e691eee
hugoecarl <mormeme@gmail.com>
2020-05-18 01:29:50 -0300

    Assembly exe

--- entrada.php
+++ entrada.php
@@ -1,5 +1,39 @@
 <?php  
-1+1;
+$x = 2;
+$y = 5;
+$i = 0;
+$n =5;
+$d = 1;
+$c1 = 0;
 
+
+  while (($i < $n) or ($i == $n)) {
+    if ($x > $y)
+      $y = $y + 1;
+    else if ($x < $y) {
+      $x = $x + 1;
+    
+    while ($c1 < 6){
+      $c2 = 0;
+      while ($c2 < 6){
+        echo 9;
+      $c2 = $c2 + 1;
+      }
+      $c1 = $c1 + 1;
+    }
+    
+    }
+    else if (($x == $y) and (true)){
+            echo 7;
+        } else { $d = $d * 2; echo $d; }
+  echo $i;
+  $i = $i + 1;
+    }   
+    echo 25 and true; 
+
+    if (false == false){
+      echo 2;
+    }
+    echo $x;
 
 ?>
